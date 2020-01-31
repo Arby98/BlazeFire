066d75c3185d6864644374c270e9c2cb8f4e44a5
enilenz <eni.odubawo@gmail.com>
2020-01-31 22:42:53 +0100

    added code to ask for permission to access users location

--- app/src/main/java/com/example/android/blazefire/MainActivity.java
+++ app/src/main/java/com/example/android/blazefire/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.android.blazefire;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            } else {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},2 );
+            }
+        }
+
     }
 
 }
