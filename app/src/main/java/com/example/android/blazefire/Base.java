61ca70cec4fe9c9a3dff1e5cd1f0396ef2f8708d
enilenz <eni.odubawo@gmail.com>
2020-01-31 13:11:02 +0100

    added onNavItemSelectedListener.

--- app/src/main/java/com/example/android/blazefire/Base.java
+++ app/src/main/java/com/example/android/blazefire/Base.java
@@ -1,11 +1,16 @@
 package com.example.android.blazefire;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
-public class Base extends AppCompatActivity {
-    //implements BottomNavigationView.OnNavigationItemSelectedListener
+public class Base extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     com.google.android.material.bottomnavigation.BottomNavigationView bottomNavigationView;
 
@@ -14,41 +19,44 @@ public class Base extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
 
+        loadFragment(new Tutorial());
+
         bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
     }
 
-//    private boolean loadFragment(Fragment fragment){
-//        if (fragment != null){
-//            getSupportFragmentManager()
-//                    .beginTransaction()
-//                    .replace(R.id.container,fragment)
-//                    .commit();
-//            return true;
-//        }
-//        return false;
-//    }
-//
-//    @Override
-//    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//        Fragment fragment = null;
-//        switch (item.getItemId()) {
-//            case R.id.action_alarm:
-//                fragment = new Alarm();
-//                Log.i("NavBar", "Alarm Clicked");
-//                break;
-//            case R.id.action_community:
-//                fragment = new Community();
-//                Log.i("NavBar", "Community Clicked");
-//                break;
-//            case R.id.action_profile:
-//                fragment = new Profile();
-//                Log.i("NavBar", "Profile Clicked");
-//                break;
-//            case R.id.action_tutorial:
-//                fragment = new Tutorial();
-//                Log.i("NavBar", "Tutorial Clicked");
-//                break;
-//        }
-//        return loadFragment(fragment);
-//   }
+    private boolean loadFragment(Fragment fragment){
+        if (fragment != null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.container,fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+        switch (item.getItemId()) {
+            case R.id.action_alarm:
+                fragment = new Alarm();
+                Log.i("NavBar", "Alarm Clicked");
+                break;
+            case R.id.action_community:
+                fragment = new Community();
+                Log.i("NavBar", "Community Clicked");
+                break;
+            case R.id.action_profile:
+                fragment = new Profile();
+                Log.i("NavBar", "Profile Clicked");
+                break;
+            case R.id.action_tutorial:
+                fragment = new Tutorial();
+                Log.i("NavBar", "Tutorial Clicked");
+                break;
+        }
+        return loadFragment(fragment);
+   }
 }
